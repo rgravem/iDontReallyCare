6c2acd644b67510d668d9fd3120cdfcf22f30f1e
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-15 14:43:10 -0500

    update table status route working

--- server/app.js
+++ server/app.js
@@ -16,4 +16,31 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve('public/index.html'));
 });
 
+app.post('/changeTableStatus', urlEncodedParser, function(req, res){
+  console.log('tableStatus hit');
+  //expecting a table and a status
+  var data = {status: req.body.status, id: req.body.id};
+  pg.connect(connectionString, function(err, client, done){
+    if (err){
+      console.log(err);
+    }
+    else{
+      client.query('UPDATE dinner_table SET status=($1) WHERE id=($2)', [data.status, data.id]);
+      var resultQuery = client.query(`SELECT * FROM dinner_table WHERE id=${data.id}`);
+      var resultArray = [];
+      resultQuery.on('row', function(row){
+        resultArray.push(row);
+      });
+      resultQuery.on('end', function(){
+        done();
+        return res.json(resultArray);
+      });
+    }
+  });
+});
+
+app.post('/changeServer', urlEncodedParser, function(req, res){
+  console.log('changeServer hit');
+});
+
 app.use(express.static('public'));
