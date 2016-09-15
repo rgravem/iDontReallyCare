f85cbda5ca7908255ea960fd3d5a6fb64a882cab
Brent Reno <brent.reno@gmail.com>
2016-09-15 15:45:57 -0500

    loading employees and tables works

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -8,22 +8,31 @@ var newEmployee ={};
 $(document).ready(function(){
   console.log('JQ works');
   //get employees and tables on load
-  $.ajax({
-      type:"get",
-      url: '/getServer',
-      success: function(data){
-        console.log("back from the server with:", data);
-        listEmployees(data);
-      }//end success
-  });// end getServer ajax
-  $.ajax({
-      type:"get",
-      url: '/getAllTables',
-      success: function(data){
-        console.log("back from the server with:", data);
-        listTables(data);
-      }//end success
-  });// end getServer ajax
+  $('#currentEmployees').on('click', function(){
+    $.ajax({
+        type:"get",
+        url: '/getServer',
+        success: function(data){
+          console.log("back from the server with:", data);
+          listEmployees(data);
+          for (var i = 0; i < data.length; i++) {
+            employees.push(data[ i ]);
+          }
+        }//end success
+    });// end getServer ajax
+  });// end on click
+
+  $('#currentTables').on('click', function(){
+
+    $.ajax({
+        type:"get",
+        url: '/getAllTables',
+        success: function(table){
+          console.log("back from the server with:", table);
+          listTables(table);
+        }//end success
+    });// end getTables ajax
+  });//end on click
 
 
 
@@ -113,44 +122,39 @@ var cycleStatus = function( index ){
 }; // end cycleStatus
 
 var listEmployees = function(data){
-  var addLineText;
-  var closing;
-  console.log( 'in listEmployees', employees );
-  $('#employeesOutput').html('<ul>');
+  var addLineText ='';
+  console.log( 'in listEmployees');
+
   // loop through the tables array and display each table
   for( i=0; i< data.length; i++ ){
     var line = data[i].first_name + " " + data[i].last_name + ', id: ' + data[ i ].id;
 
     //add line text
     addLineText += '<li>' + line + '</li>';
-    // add line to output div
-    $('#employeesOutput').html(addLineText);
   }
-  // add closing UL text
-  closing += '</ul>';
-  //add closing UL
-  $('#employeesOutput').html(closing);
-  // update tables display
-  listTables();
+  //update display employees
+  $('#employeesOutput').html("<ul>"+addLineText + "</ul>");
+
 }; // end listEmployees
 
-var listTables = function(data){
+var listTables = function(table){
   var outputText;
   console.log( "in listTables" );
-  // target our output div
-  $('#tablesOutput').html("");
+
   // loop through the tables array and display each table
 
   // select to assign a server to this table
+  console.log("employees array in display tables:", employees);
   var selectText = '<select>';
   for (var i = 0; i < employees.length; i++) {
-    selectText+= '<option value=' + i + '>'+ employees[i].firstName + ' ' + employees[i].lastName + '</option>';
+    selectText+= '<option value=' + i + '>'+ employees[i].first_name + ' ' + employees[i].last_name + '</option>';
   }
   selectText += '</select>';
   // display employees
-  for( i=0; i< data.length; i++ ){
+  console.log("data before for loop", table);
+  for( i=0; i< table.length; i++ ){
     // status is a button that, when clicked runs cycleStatus for this table
-    var line = data[i].name + " - capacity: " + data[i].capacity + ', server: ' + selectText + ', status: <button onClick="cycleStatus(' + i + ')">' + data[i].status + "</button>";
+    var line = table[i].table_name + " - capacity: " + table[i].capacity + ', server: ' + selectText + ', status: <button onClick="cycleStatus(' + i + ')">' + table[i].status + "</button>";
     // add line to output div
     outputText += '<p>' + line + '</p>';
     $('#tablesOutput').html(outputText);
