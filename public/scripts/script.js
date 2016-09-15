7b5bf630fc7c64ec02130289428fe35ee73fbfa5
Brent Reno <brent.reno@gmail.com>
2016-09-15 14:53:38 -0500

    update doc ready with server and table get routes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,16 +2,56 @@ console.log('script sourced');
 
 // arrays
 var tables=[];
-var employees=[];
+ var employees=[];
+var newEmployee ={};
 
 $(document).ready(function(){
   console.log('JQ works');
+  //get employees and tables on load
+  $.ajax({
+      type:"get",
+      url: '/getServer',
+      success: function(data){
+        console.log("back from the server with:", data);
+        listEmployees(data);
+      }//end success
+  });// end getServer ajax
+  $.ajax({
+      type:"get",
+      url: '/getAllTables',
+      success: function(data){
+        console.log("back from the server with:", data);
+        listTables(data);
+      }//end success
+  });// end getServer ajax
+
+
 
   // add employee on click
-  $('#createEmployee').on('click',)
+  $('#createEmployee').on('click',function(){
+    //verify click is working
+    console.log("in createEmployee on click");
+    createEmployee();
+    //send employee info to the server
+    $.ajax({
+      type:"POST",
+      url: '/addServer',
+      data: newEmployee,
+      success: function(data){
+        console.log("ajax success back with:", data);
+        //display  employees in employeesOutput
+        listEmployees(data);
+        // push into employees array
+        employees.push(data);
+      }//end success
+    });//end ajax /addServer call
+  });//end createEmployee on click
 
   //add table on click
-$('#createTable')
+$('#createTable').on('click', function(){
+  //verify click is working
+  console.log("in createTable on click");
+});
   //display current employees
 
   // display current tables
@@ -24,14 +64,11 @@ var createEmployee = function(){
   var employeeFirstName = $( '#employeeFirstNameIn' ).val();
   var employeeLastName = $( '#employeeLastNameIn' ).val();
   // create object for employee
-  var newEmployee= {
+  newEmployee= {
     firstName : employeeFirstName,
     lastName : employeeLastName
   }; // end object
-  // push into employees array
-  employees.push( newEmployee );
-  // update display
-  listEmployees();
+
 }; // end createEmployee
 
 var createTable = function(){
@@ -75,14 +112,14 @@ var cycleStatus = function( index ){
   listTables();
 }; // end cycleStatus
 
-var listEmployees = function(){
+var listEmployees = function(data){
   var addLineText;
   var closing;
   console.log( 'in listEmployees', employees );
   $('#employeesOutput').html('<ul>');
   // loop through the tables array and display each table
-  for( i=0; i< employees.length; i++ ){
-    var line = employees[i].firstName + " " + employees[i].lastName + ', id: ' + i;
+  for( i=0; i< data.length; i++ ){
+    var line = data[i].first_name + " " + data[i].last_name + ', id: ' + data[ i ].id;
 
     //add line text
     addLineText += '<li>' + line + '</li>';
@@ -97,11 +134,11 @@ var listEmployees = function(){
   listTables();
 }; // end listEmployees
 
-var listTables = function(){
+var listTables = function(data){
   var outputText;
   console.log( "in listTables" );
   // target our output div
-  document.getElementById('tablesOutput').innerHTML = '';
+  $('#tablesOutput').html("");
   // loop through the tables array and display each table
 
   // select to assign a server to this table
@@ -111,9 +148,9 @@ var listTables = function(){
   }
   selectText += '</select>';
   // display employees
-  for( i=0; i< tables.length; i++ ){
+  for( i=0; i< data.length; i++ ){
     // status is a button that, when clicked runs cycleStatus for this table
-    var line = tables[i].name + " - capacity: " + tables[i].capacity + ', server: ' + selectText + ', status: <button onClick="cycleStatus(' + i + ')">' + tables[i].status + "</button>";
+    var line = data[i].name + " - capacity: " + data[i].capacity + ', server: ' + selectText + ', status: <button onClick="cycleStatus(' + i + ')">' + data[i].status + "</button>";
     // add line to output div
     outputText += '<p>' + line + '</p>';
     $('#tablesOutput').html(outputText);
