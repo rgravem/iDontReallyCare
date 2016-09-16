f5b33b3680de6a3154da82599d55c5f3c7547c57
Nate Biessener <nathaniel.biessener@gmail.com>
2016-09-16 14:07:41 -0500

    update dinner_table create script

--- dbScript.sql
+++ dbScript.sql
@@ -1,3 +1,3 @@
 CREATE TABLE server (id SERIAL PRIMARY KEY, first_name VARCHAR(32), last_name VARCHAR(32));
 
-CREATE TABLE dinner_table (id SERIAL PRIMARY KEY, table_name VARCHAR(12), capacity INTEGER, status VARCHAR(12), server_id INT REFERENCES server(id));
+CREATE TABLE dinner_table (id SERIAL PRIMARY KEY, table_name VARCHAR(12), capacity INTEGER, status VARCHAR(12), server_id INT REFERENCES server(id) ON DELETE SET NULL);
